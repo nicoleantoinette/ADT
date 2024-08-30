9aed79bb67600a79bb97a977bcaf947399bc151e
nicoleantoinette <nicole.antoinette.29@gmail.com>
2024-08-30 23:55:23 +0800

    Initial commit

---
+++ src/component/User/User.jsx
@@ -0,0 +1,11 @@
+function User() {
+    return (
+        <div>
+           <h1> Nicole Antoinette Dela Cruz</h1>
+           <h2>BSIT-3B</h2>
+           <h3>Hi! I am a student programmer.</h3>
+        </div>
+    );
+}
+
+export default User;
